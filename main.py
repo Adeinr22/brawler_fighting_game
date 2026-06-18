ec17e4e99b42585a9ff5259c47f5326e8ea2f923
Adeinr22 <andrei05.jules22@gmail.com>
2026-06-19 01:31:44 +0800

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawler")
 
+# load background image
+bg_image = pygame.image.load("assets/images/background/background.jpg")
+
 # game loop
 run = True
 while run:
