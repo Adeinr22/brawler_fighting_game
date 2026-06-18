d6b8197ac4a5a7958a46888534fdefdfca4d9542
Adeinr22 <andrei05.jules22@gmail.com>
2026-06-19 01:38:38 +0800

    Enhance background rendering by converting image and scaling it to fit the screen

--- main.py
+++ main.py
@@ -10,16 +10,26 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brawler")
 
 # load background image
-bg_image = pygame.image.load("assets/images/background/background.jpg")
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+# drawing background
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0,0))
 
 # game loop
 run = True
 while run:
 
+    # draw background
+    draw_bg()
     # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # update display
+    pygame.display.update()
 
 # exit pygame
 pygame.quit()
