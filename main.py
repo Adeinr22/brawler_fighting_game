0d84a51be81ddf7338d170d5dfcf18dd812927cb
Adeinr22 <andrei05.jules22@gmail.com>
2026-06-19 17:17:38 +0800

    Add Turok font file to assets/fonts directory
    - Introduced the Turok font (version 2.10) by Neale Davidson.
    - The font file is now available for use in the project.
    - All rights reserved for the font.

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ WHITE = (255, 255, 255)
 # game variables
 intro_count = 3
 last_count_update = pygame.time.get_ticks()
+score = [0, 0]
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
 
 # fighter variables
 WARRIOR_SIZE = 162
@@ -39,9 +42,20 @@ bg_image = pygame.image.load("assets/images/background/background.jpg").convert_
 warrior_sheet = pygame.image.load("assets/images/warrior/sprites/warrior.png").convert_alpha()
 wizard_sheet = pygame.image.load("assets/images/wizard/sprites/wizard.png").convert_alpha()
 
+# victory image
+victory_img = pygame.image.load("assets/images/icons/victory.png").convert_alpha()
 # number of steps in each animation
 WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
 WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+
+# font
+count_font = pygame.font.Font("assets/fonts/turok.ttf", 80)
+score_font = pygame.font.Font("assets/fonts/turok.ttf", 30)
+
+# draw text
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 # drawing background
 def draw_bg():
     scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -70,16 +84,20 @@ while run:
     # show player stats
     draw_health_bar(fighter_1.health, 20, 20)
     draw_health_bar(fighter_2.health, 580, 20)
+    draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
+    draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
 
     # countdown
     if intro_count <= 0:
         # move fighters
-        fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
-        fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1)
+        fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
+        fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
     else:
+        draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
         if (pygame.time.get_ticks() - last_count_update) >= 1000:
             intro_count -= 1
             last_count_update = pygame.time.get_ticks()
+            
 
     # update fighters
     fighter_1.update()
@@ -88,6 +106,24 @@ while run:
     # draw fighters
     fighter_1.draw(screen)
     fighter_2.draw(screen)
+
+    # player defeat
+    if round_over == False:
+        if fighter_1.alive == False:
+            score[1] += 1
+            round_over = True
+            round_over_time = pygame.time.get_ticks()
+        elif fighter_2.alive == False:
+            score[0] += 1
+            round_over = True
+            round_over_time = pygame.time.get_ticks()
+    else:
+        screen.blit(victory_img, (360, 150))
+        if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+            round_over = False
+            intro_count = 3
+            fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
+            fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
 
     # event handler
     for event in pygame.event.get():
