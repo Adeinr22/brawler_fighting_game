f06820cb917752591fc2d0f92fde5ebee5d20ec3
Adeinr22 <andrei05.jules22@gmail.com>
2026-06-19 18:27:40 +0800

    Add wizard file and Wizard class with input handling for player controls

---
+++ wizard.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Wizard(Fighter): # Inherits from Fighter parent class
+    def handle_input(self, key, speed, target):
+        dx = 0
+        # Player 2 (Wizard) Controls layout
+        if key[pygame.K_LEFT]:
+            dx = -speed
+            self.running = True
+        if key[pygame.K_RIGHT]:
+            dx = speed
+            self.running = True
+        if key[pygame.K_UP] and self.jump == False:
+            self.vel_y = -30
+            self.jump = True
+        if key[pygame.K_KP1] or key[pygame.K_KP2]:
+            self.attack(target)
+            if key[pygame.K_KP1]:
+                self.attack_type = 1
+            if key[pygame.K_KP2]:
+                self.attack_type = 2
+        return dx
