48f153203bbc3eb6be6f526229d1771fc8211c43
Adeinr22 <andrei05.jules22@gmail.com>
2026-06-19 18:25:37 +0800

    Add warrior file and Warrior class with input handling for player controls

---
+++ warrior.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Warrior(Fighter): # Inherits from Fighter parent class
+    def handle_input(self, key, speed, target):
+        dx = 0
+        # Player 1 (Warrior) Controls layout
+        if key[pygame.K_a]:
+            dx = -speed
+            self.running = True
+        if key[pygame.K_d]:
+            dx = speed
+            self.running = True
+        if key[pygame.K_w] and self.jump == False:
+            self.vel_y = -30
+            self.jump = True
+        if key[pygame.K_r] or key[pygame.K_t]:
+            self.attack(target)
+            if key[pygame.K_r]:
+                self.attack_type = 1
+            if key[pygame.K_t]:
+                self.attack_type = 2
+        return dx
